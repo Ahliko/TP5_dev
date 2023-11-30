3038a4ca1516c40fa22702405205e935a2a77391
ahliko <killian.guillemot@ynov.com>
2023-11-30 11:00:52 +0100

    Add tp5_enc_client_2.py && tp5_enc_server_2.py

---
+++ tp5_enc_client_2.py
@@ -0,0 +1,79 @@
+import socket
+
+
+def binaire(msg):
+    return msg.to_bytes((msg.bit_length() + 7) // 8, byteorder='big')
+
+
+def unbinaire(msg):
+    return int.from_bytes(msg, byteorder='big')
+
+
+def send(lst, signe):
+    header_int = len(binaire(lst[0])).to_bytes(2, byteorder='big')
+    header = len(header_int + binaire(lst[0]) + signe.encode() + binaire(lst[1])).to_bytes(2, byteorder='big')
+    seq_fin = "<clafin>".encode()
+    return header + header_int + binaire(lst[0]) + signe.encode() + binaire(lst[1]) + seq_fin
+
+
+def receive(sckt):
+    data = skt.recv(2)
+    if data == b"":
+        return
+    msg_len = int.from_bytes(data[0:2], byteorder='big')
+    chunks = []
+    bytes_received = 0
+    while bytes_received < msg_len:
+        chunk = skt.recv(min(msg_len - bytes_received,
+                                1024))
+        if not chunk:
+            raise RuntimeError('Invalid chunk received bro')
+        chunks.append(chunk)
+        bytes_received += len(chunk)
+    fin = skt.recv(8)
+    if fin.decode() != "<clafin>":
+        raise RuntimeError('Invalid chunk received bro')
+    else:
+        return b"".join(chunks)
+
+
+msg = input("Calcul à envoyer: ")
+msg = msg.replace(" ", "")
+if msg.__contains__("+"):
+    signe = "+"
+elif msg.__contains__("-"):
+    signe = "-"
+elif msg.__contains__("*"):
+    signe = "*"
+else:
+    print("Ce n'est pas un bon signe")
+    exit(1)
+
+if msg.__contains__(signe):
+    msg_lst = msg.split(signe)
+    lst = [0, 0]
+    if len(msg_lst) != 2:
+        print("Erreur de saisie")
+        exit(1)
+    try:
+        lst[0] = int(msg_lst[0])
+        lst[1] = int(msg_lst[1])
+    except ValueError:
+        print("Erreur de conversion")
+        exit(1)
+    for i in lst:
+        if len(binaire(i)) > 4:
+            print("Nombre trop grand")
+            exit(1)
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(('127.0.0.1', 9999))
+
+    s.send(send(lst, signe))
+
+else:
+    exit(1)
+
+s_data = unbinaire(receive(s))
+print(f"Résultat: {s_data}")
+s.close()
---
+++ tp5_enc_server_2.py
@@ -0,0 +1,77 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind(('127.0.0.1', 9999))
+
+s.listen(1)
+conn, addr = s.accept()
+
+
+def unbinaire(msg):
+    return int.from_bytes(msg, byteorder='big')
+
+
+def binaire(msg):
+    return msg.to_bytes((msg.bit_length() + 7) // 8, byteorder='big')
+
+
+def send(result):
+    header = len(binaire(result)).to_bytes(2, byteorder='big')
+    seq_fin = "<clafin>".encode()
+    return header + binaire(result) + seq_fin
+
+
+def receive():
+    data = conn.recv(2)
+    if data == b"":
+        return
+    msg_len = int.from_bytes(data[0:2], byteorder='big')
+
+    print(f"Lecture des {msg_len} prochains octets")
+
+    # Une liste qui va contenir les données reçues
+    chunks = []
+
+    bytes_received = 0
+    while bytes_received < msg_len:
+        # Si on reçoit + que la taille annoncée, on lit 1024 par 1024 octets
+        chunk = conn.recv(min(msg_len - bytes_received,
+                              1024))
+        if not chunk:
+            raise RuntimeError('Invalid chunk received bro')
+
+        # on ajoute le morceau de 1024 ou moins à notre liste
+        chunks.append(chunk)
+
+        # on ajoute la quantité d'octets reçus au compteur
+        bytes_received += len(chunk)
+    fin = conn.recv(8)
+    if fin.decode() != "<clafin>":
+        raise RuntimeError('Invalid chunk received bro')
+    else:
+        # ptit one-liner pas combliqué à comprendre pour assembler la liste en un seul message
+
+        return b"".join(chunks)
+
+
+while True:
+    try:
+        # On reçoit le calcul du client
+        message_received = receive()
+        if message_received is None:
+            break
+        first_header = int.from_bytes(message_received[0:2], byteorder='big')
+        first_int = unbinaire(message_received[2: 2 + first_header])
+        old = 2 + first_header
+        signe = message_received[old:old + 1].decode()
+        old += 1
+        second_int = unbinaire(message_received[old:])
+        res = eval(str(first_int) + signe + str(second_int))
+        conn.send(send(res))
+
+    except socket.error:
+        print("Error Occured.")
+        break
+
+conn.close()
